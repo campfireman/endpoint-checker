b8c14302de43370edf8349797378bc29b99989d3
Ture Claussen <ture.cl.business@gmail.com>
2022-06-21 01:14:29 +0200

    Refactored prototype

---
+++ README.md
@@ -0,0 +1,7 @@
+# endpoint-checker
+
+```bash
+py main.py 2> /dev/null > responses.csv
+```
+
+![example](doc/example.png)
--- main.py
+++ main.py
@@ -6,25 +6,25 @@ import numpy as np
 import requests
 
 INTERVAL_IN_S=5
+URLS = ["https://google.de", "https://heise.de", "http://localhost:8080"]
 
-urls = ["https://google.de", "https://heise.de", "http://localhost:8080"]
-timestamps = []
+def main():
+    fig, ax = plt.subplots()
+    ax.set_yticklabels(URLS)
+    ax.set_yticks(np.arange(0.5, len(URLS), 1))
 
-print("timestamp," + ",".join(urls))
+    timestamps = []
+    x = [0]
+    i = 0
 
-fig, ax = plt.subplots()
-ax.set_yticklabels(urls)
-ax.set_yticks(np.arange(0.5, len(urls), 1))
-x = [0]
-i = 0
-main():
+    print("timestamp," + ",".join(URLS))
     while True:
         time = datetime.now().strftime("%H:%M:%S")
         timestamps.append(time)
         ax.set_xticks(x)
         ax.set_xticklabels(timestamps)
         colors = []
-        for n, endpoint in enumerate(urls):
+        for n, endpoint in enumerate(URLS):
             try:
                 response = requests.get(endpoint)
                 color = "green" if response.status_code == 200 else "red"
